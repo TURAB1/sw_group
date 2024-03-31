691949990ceca955c420748f213cbe91a1fde9eb
hgf <fhfgh>
2024-04-01 00:02:29 +0900

    searchBar added

--- screens/announcement.tsx
+++ screens/announcement.tsx
@@ -1,15 +1,147 @@
-import { SafeAreaView, StyleSheet, Text } from "react-native"
+import { SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native"
 import { HeaderComponent } from "../components/headerComponent";
 
 export const AnnouncementScreen=(navigation:any,screenName:any)=>{
+
+  // interface Props2{
+  //   day:any
+  //   icon:any
+  //   maxTemp:any
+  //   minTemp:any 
+  //   details:any
+  // }
+
+  // const ForecastComingDays:React.FC<Props2>=({day,icon,maxTemp,minTemp,details})=>{
+  //   return(
+  //         <View  style={styles.weatherStyle}>
+  //           <Text>{new Date(day).toLocaleDateString("en-EN", { weekday: 'long' })}</Text> 
+  //           <Text>{new Date(day).getMonth()+1}/{new Date(day).getDate()}</Text>    
+  //           <Image 
+  //             style={styles.weatherIcon}
+  //             source={{uri:"https:"+icon}}/>
+  //           <Text>Max Temp:{maxTemp}°C</Text> 
+  //           <Text>Min Temp:{minTemp}°C</Text>  
+  //           <Text>{details}</Text>
+  //         </View>
+  //     );
+  // }
+
+  // const getComingDaysForecast=()=>{
+
+  //   if (data !==null){
+  //    return  data.forecast.forecastday.map((item:any, index:any) => {
+  //      if(index!==0){ 
+        
+  //       return (
+  //       <ForecastComingDays key={index} day={item.date} icon={item.day.condition.icon} maxTemp={item.day.maxtemp_c} minTemp={item.day.mintemp_c} details={item.day.condition.text}/>
+  //           );
+
+  //         }
+    
+  //     });
+  //   }
+  // }
   return(
-    <SafeAreaView>
+    <SafeAreaView style={styles.announcementSection}>
       <HeaderComponent navigation={navigation} screenName="공지사항"/>
-      <Text>Announcements</Text>
-    </SafeAreaView>
+      <View style={styles.topButtonSection}>
+        <TouchableOpacity >
+           <View style={styles.allButton}>
+            <Text style={styles.allButtonText}>전체</Text>
+           </View> 
+        </TouchableOpacity>
+        <TouchableOpacity >
+          <View style={styles.button}>
+              <Text>공지</Text>
+          </View> 
+        </TouchableOpacity>  
+        <TouchableOpacity >
+          <View style={styles.button}>
+              <Text>인사</Text>
+          </View> 
+        </TouchableOpacity> 
+        <TouchableOpacity >
+          <View style={styles.button}>
+              <Text>근태</Text>
+          </View> 
+        </TouchableOpacity>  
+        <TouchableOpacity >
+          <View style={styles.button}>
+              <Text>연락망</Text>
+          </View> 
+        </TouchableOpacity> 
+     </View>
+     <View 
+        style={styles.searchSection}>
+          <TextInput
+            style={styles.searchInput}
+            //onChangeText={setContent}
+            // value={content}
+            placeholder='검색어'
+            keyboardType="ascii-capable" />
+          <TouchableOpacity >
+            <View style={styles.searchButton}>
+              <Text style={styles.searchButtonText}>검색</Text>
+            </View> 
+          </TouchableOpacity>
+      </View>
+    </SafeAreaView> 
   )
 
 }
 const styles=StyleSheet.create({
+  announcementSection:{
+    marginLeft:10,
+    marginRight:10
+  },
+  topButtonSection:{
+    flexDirection:"row",
+    justifyContent:"space-between",
+    marginTop:10
 
+  },
+  allButton:{
+    height:30,
+    width:60,
+    borderRadius:5,
+    alignItems:"center",
+    justifyContent:"center",
+    backgroundColor:"#004A94",
+   
+  },
+  allButtonText:{
+    color:"white",
+    fontWeight:"bold",
+  },
+  button:{
+    alignItems:"center",
+    justifyContent:"center",
+    height:30,
+    width:60,
+    borderRadius:5,
+    borderWidth:1,
+    borderColor:"gray",  
+  },
+  searchSection:{
+    marginTop: 10,
+    flexDirection:"row",
+  },
+  searchInput:{
+    backgroundColor:"white",
+    width:"83%",
+    height:40
+  },
+  searchButton:{
+    height:40,
+    width:60,
+    borderRadius:5,
+    alignItems:"center",
+    justifyContent:"center",
+    backgroundColor:"#004A94",
+   
+  },
+  searchButtonText:{
+    color:"white",
+    fontWeight:"bold",
+  },
 });
--- screens/staffContacts.tsx
+++ screens/staffContacts.tsx
@@ -1,17 +1,99 @@
-import { StyleSheet, Text, View,SafeAreaView} from "react-native"
+
+import * as React from "react";
+import { StyleSheet, Text, View,SafeAreaView, TouchableOpacity, TextInput} from "react-native"
 import { HeaderComponent } from "../components/headerComponent";
+import Entypo from "react-native-vector-icons/Entypo";
+import Feather from "react-native-vector-icons/Feather"
+// import{ Searchbar } from "react-native-paper";
+// import { SearchBar } from "react-native-elements";
+import { SearchBar } from "@rneui/themed";
 
 
 
 export const StaffContactsScreen=(navigation:any,screenName:any)=>{
+  const [search, setSearch] = React.useState("");
   return(
-   <SafeAreaView>
+   <SafeAreaView style={styles.staffContacts}>
     <HeaderComponent navigation={navigation} screenName="회사연락망"/>
-    <Text>staff contacts</Text>
+    <View style={styles.topButtonSection}>
+        <TouchableOpacity >
+           <View style={styles.swButton}>
+            <Text style={styles.swButtonText}>애드피아</Text>
+           </View> 
+        </TouchableOpacity>
+        <TouchableOpacity >
+          <View style={styles.mallButton}>
+              <Text>몰</Text>
+          </View> 
+        </TouchableOpacity>  
+        <TouchableOpacity >
+          <View style={styles.mallButton}>
+              <Text>푸드몰</Text>
+          </View> 
+        </TouchableOpacity> 
+        <TouchableOpacity >
+          <View style={styles.star}>
+              <Entypo name="star" size={30} color={"#004A94"}/>
+          </View> 
+        </TouchableOpacity> 
+     </View>
+      <View style={styles.searchSection}>
+        <Feather name="search" size={20} color="black" 
+        style={{marginLeft:1,marginRight:4}}/>
+        <TextInput style={{fontSize:15}} placeholder="검색하기"/>
+      </View>
     </SafeAreaView> 
   )
 
 }
 const styles=StyleSheet.create({
+  staffContacts:{
+    marginLeft:10,
+    marginRight:10
+  },
+  topButtonSection:{
+    flexDirection:"row",
+    justifyContent:"space-between",
+    marginTop:10
 
+  },
+  swButton:{
+    height:40,
+    width:90,
+    borderRadius:5,
+    alignItems:"center",
+    justifyContent:"center",
+    backgroundColor:"#004A94",
+   
+  },
+  swButtonText:{
+    color:"white",
+    fontWeight:"bold",
+  },
+  mallButton:{
+    alignItems:"center",
+    justifyContent:"center",
+    height:40,
+    width:90,
+    borderRadius:5,
+    borderWidth:1,
+    borderColor:"gray",
+    
+  },
+  star:{
+    alignItems:"center",
+    justifyContent:"center",
+    height:40,
+    width:90,
+
+  },
+  searchSection:{
+    flexDirection:"row",
+    alignItems:"center",
+    backgroundColor:"#D1D1D1",
+    borderRadius:5,
+    padding:10,
+    marginTop:10,
+    height:40
+  }
 });
